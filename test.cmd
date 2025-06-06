00a4076291e043429f38683874d96e49048f645d
Travnicek <TravnicekMi@vda.cz>
2025-06-06 09:14:09 +0200

    Toto je druha verze

--- test.cmd
+++ test.cmd
@@ -6,22 +6,31 @@ set /p prvni=Zadej prvni cislo:
 set /p druhy=Zadej druhe cislo:
 
 
-set /p operace=Jako chces operaci? */+//
+set /p operace=Jako chces operaci? */+//:
 
 REM pocitani
 if %operace%==* (
-    set /a nasobeni=%prvni%*%druhy%)
-echo Vysledek je %nasobeni%
-goto start
+    set /a nasobeni=%prvni%*%druhy%
+goto s)
 
 if %operace%==+ (
-    set /a soucet=%prvni%+%druhy%)
-echo Vysledek je %soucet%
-goto start
+    set /a soucet=%prvni%+%druhy%
+goto n)
 
 if %operace%==/ (
-    set /a deleni=%prvni%/%druhy%)
+    set /a deleni=%prvni%/%druhy% 
+    goto d)
+
+REM Vypocty
+:s
+echo Vysledek je %nasobeni%
+goto start
+:n
+echo Vysledek je %soucet%
+goto start
+:d
 echo Vysledek je %deleni%
 goto start
+
 
 pause
