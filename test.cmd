4bef7777c95e02ccc2ae02ba46f6299e2640446d
Travnicek <TravnicekMi@vda.cz>
2025-06-06 09:17:54 +0200

    Toto je treti verze

--- test.cmd
+++ test.cmd
@@ -1,14 +1,14 @@
 @echo off
-
+REM Tady jsem si zadal aby se mi po kazdem vypoctu program opakoval
 :start
 REM Nacitani cisel
 set /p prvni=Zadej prvni cislo:
 set /p druhy=Zadej druhe cislo:
 
-
+REM zadal jsem si promenou operace aby si uzivatel vybral jakou chce
 set /p operace=Jako chces operaci? */+//:
 
-REM pocitani
+REM pocitani, po kazdem vypoctu mam goto a nekam aby to fungovalo
 if %operace%==* (
     set /a nasobeni=%prvni%*%druhy%
 goto s)
@@ -21,7 +21,7 @@ if %operace%==/ (
     set /a deleni=%prvni%/%druhy% 
     goto d)
 
-REM Vypocty
+REM Vypocty, Tady se nam ukazou vypocty operace ktere si uzivatel zvolil
 :s
 echo Vysledek je %nasobeni%
 goto start
